66f1db898b694d098ae9840cff4b82c2c3aeffbf
mikeyyyzhao <mchlzhao@gmail.com>
2019-01-22 22:21:38 -0800

    Added ability to click on Google o-auth

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 class LoginScreen extends Component {
+
+  signInWithGoogleAsync = async() => {
+       try {
+         const result = await Expo.Google.logInAsync({
+           behavior: 'web',
+           iosClientId: '271681026569-7i9tfesh02c4kl8u1uv1n938ncoc2ehp.apps.googleusercontent.com',
+           scopes: ['profile', 'email'],
+         });
+
+         if (result.type === 'success') {
+           return result.accessToken;
+         } else {
+           return {cancelled: true};
+         }
+       } catch(e) {
+         return {error: true};
+       }
+     }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>LoginScreen</Text>
+        <Button
+          title="Sign in with Google"
+          onPress={()=>this.signInWithGoogleAsync()}
+        />
       </View>
     );
   }
